5aca6218ea79a05544d355d65464719f7bed7172
Arpita Patel <125717956+arpitaapatel@users.noreply.github.com>
2025-08-20 06:52:19 +0000

    Commit

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import search from "./search.svg";
+import menu from "./menu.svg";
+import loader from "./loader.svg";
+import records from "./records.svg";
+import screening from "./screening.svg";
+import user from "./user.svg";
+import apps from "./apps.svg";
+export { search, menu, loader, records, screening, user, apps };
